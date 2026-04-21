c25e5a7f0b046427a7a0c550f33430a3aec1ba8d
Pranzoal <dangolprakash7@gmail.com>
2026-04-21 10:44:26 +0930

    fixes 5.9

--- add_button_new.js
+++ add_button_new.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         QuickBooks Invoice Print + Pick Slip (New UI Table Fix)
 // @namespace    http://tampermonkey.net/
-// @version      5.8
-// @description  Print / Pick Slip for QuickBooks invoice editor using exact field selectors and header-mapped columnn
+// @version      5.9
+// @description  Print / Pick Slip for QuickBooks invoice editor using exact field selectors and header-mapped column and spaces
 // @author       Raj - Gorkhari
 // @match        https://qbo.intuit.com/*
 // @include      https://qbo.intuit.com/app/invoice?*
